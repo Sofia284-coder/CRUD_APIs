5d2270dcde9c81ea53f2353f650c0f82600c7ee0
Sofia284-coder <sofiaqureshi912@gmail.com>
2026-07-29 02:48:46 +0500

    Stage 5: Swagger UI

--- src/app.js
+++ src/app.js
@@ -3,16 +3,17 @@ const app = express();
 const port = 3000;
 
 import CRUDRoutes from "./routes/CRUDRoutes.js"
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: "json" };
 
 //middleware
 app.use(express.json())
 
+app.use("/docs", swaggerUi.serve);
+app.get("/docs", swaggerUi.setup(swaggerDocument));
 
 //routes
 app.use('/', CRUDRoutes)
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
